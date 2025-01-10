26e3f3843c6cabd1f65bb1d3cdfd1a6c8a4b1ba9
Noah <bigspaceships56@gmail.com>
2025-01-10 00:05:06 -0500

    FEAT: working on bar and stuff not dying on boot

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,14 +1,27 @@
 return {
     {
-        "uga-rosa/ccc.nvim",
-        opts = {
-            auto_enable = true,
-            lsp = true,
-            recognize = {
-                -- input = true,
-                -- output = true,
-            },
-        },
+        "BigSpaceships/ccc.nvim",
+        lazy = false,
+        config = function()
+            local ccc = require("ccc")
+
+            local opts = {
+                auto_enable = true,
+                lsp = true,
+                recognize = {
+                    -- input = true,
+                    -- output = true,
+                },
+                outputs = {
+                    ccc.output.hex,
+                    ccc.output.css_rgb,
+                    ccc.output.css_rgb_commas,
+                    ccc.output.css_hsl,
+                }
+            }
+
+            ccc.setup(opts)
+        end,
         keys = {
             {
                 "<leader>c",
--- .zshrc
+++ .zshrc
@@ -110,3 +110,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 export PATH="$PATH:/opt/nvim-linux64/bin"
+export PATH="$PATH:~/software-git/idea"
+
+alias clone='rclone mount google-drive:APCSWorkspaces/ drive/apcs/'
